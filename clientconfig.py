5d74c24de146453be82d6c3357055f5bb1cfb715
christian-hawk <eland.christian@gmail.com>
2020-04-14 14:31:41 -0300

    fixed untracked

--- clientconfig.py
+++
@@ -1,9 +0,0 @@
-CLIENT_ID = "8d05ef28-ede9-49a9-886a-028c66f0448c"
-CLIENT_SECRET = "novanovidade"
-AUTH_URI = "https://christian-XPS-8700/oxauth/restv1/authorize"
-SCOPE = "openid profile"
-RESPONSE_TYPE = "code"
-REDIRECT_URI = "https://localhost:5000/callback"
-TOKEN_ENDPOINT = "https://christian-xps-8700/oxauth/restv1/token"
-USERINFO = 'https://christian-XPS-8700/oxauth/restv1/userinfo'
-SSL_VERIFY = False
